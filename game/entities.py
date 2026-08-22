814cca9123c981cf6f42af755285dfca7ef78164
onequbit <onequbit@yahoo.com>
2026-08-22 14:20:34 -0400

    increased bullet lifespan

--- game/entities.py
+++ game/entities.py
@@ -216,7 +216,7 @@ class Bullet(PhysicalObject):
     def __init__(self, *args, **kwargs):
         super(Bullet, self).__init__(
             resources.bullet_image, *args, **kwargs)
-        pyglet.clock.schedule_once(self.die, 1)
+        pyglet.clock.schedule_once(self.die, 1.5)
 
     def die(self, dt):
         self.dead = True
